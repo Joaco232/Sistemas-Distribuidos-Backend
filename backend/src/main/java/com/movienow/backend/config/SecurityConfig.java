1aba4585dddcf5cfec25b09c5f40448ed428f492
ElNelsonn <santbord@gmail.com>
2025-11-14 18:12:49 -0300

    Working2

--- backend/src/main/java/com/movienow/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/movienow/backend/config/SecurityConfig.java
@@ -83,6 +83,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "http://localhost:3001")); // Ambos puertos
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE","PATCH", "OPTIONS"));
         configuration.setAllowedOrigins(Arrays.stream(corsAllowedOrigins.split(",")).map(String::trim).toList());
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept", "Origin", "X-Requested-With", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
